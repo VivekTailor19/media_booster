9b70a9484f3b33cfacb5f72688c04ef74a670921
Vivek <tailorvivekgec19@gmail.com>
2023-05-11 11:10:24 +0530

    media booster

--- lib/login_UI/screens/createpage.dart
+++ lib/login_UI/screens/createpage.dart
@@ -47,15 +47,32 @@ class _Login_CreateState extends State<Login_Create> {
                     child: FlutterLogo(size: 25.w,)),
               ),
             ),
-            Column(
-              
+            Align(
+              alignment: Alignment(0,-0.15),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Create",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 60,color: Color(0xff1F6AC7),letterSpacing: 2),),
+                  Text("account",style: TextStyle(fontSize:33,color: Color(0xff2A8CED),),),
+                  SizedBox(height: 2.h,),
+                  Text("data"),
+                  TextField(
+                    decoration: InputDecoration(
+
+                    ),
+                  ),
+                ],
+
+              ),
             ),
             Align(
               alignment: Alignment(0.5,0.9),
               child: Row(mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Already have account?",style: TextStyle(fontSize: 5.w),),
-                  Text("Sign in",style: TextStyle(color: Colors.blue,),)
+                  Text("Already have account?",style: TextStyle(fontSize: 4.5.w),),
+                  SizedBox(width: 1.5.w,),
+                  Text("Sign in",style: TextStyle(color: Colors.blue,fontSize: 4.5.w),)
                 ],
               ),
             )
